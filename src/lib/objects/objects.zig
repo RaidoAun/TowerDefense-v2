ce5499d2fe4a5e068a15b4d1022668385401b314
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-07-15 01:29:54 +0300

    rename bubble to bullet
    don't want to delete it either, probably will need it anyway

--- src/lib/objects/objects.zig
+++ src/lib/objects/objects.zig
@@ -24,16 +24,17 @@ pub const Player = struct {
         }
     }
 };
-
-pub const Bubble = struct {
+// TODO complete this
+pub const Bullet = struct {
+    const Self = @This();
     shape: Circle,
     speed: f32,
-    pub fn update(self: *Bubble, dt: f64) void {
+    pub fn update(self: *Self, dt: f64) void {
         const dSpeed: i32 = @intFromFloat(self.speed * dt);
         self.shape.y += dSpeed;
     }
 
-    pub fn onCollision(self: *Bubble) void {
+    pub fn onCollision(self: *Self) void {
         self.shape.y -= 1000;
     }
 };
