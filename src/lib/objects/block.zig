8bbfca0d7376026510787cec5e275b8a05056e66
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-07-16 18:32:07 +0300

    add TODO

--- src/lib/objects/block.zig
+++ src/lib/objects/block.zig
@@ -8,6 +8,10 @@ pub const Type = enum {
 pub const Block = struct {
     const Self = @This();
     // TODO do not use Rectangle here
+    // to fully optimize this for memory only the type is needed
+    // if we assume width to be a constant then positions can be
+    // determined from the indexes at which they are in the maps
+    // slice of Blocks
     shape: shapes.Rectangle,
     type: Type,
 
