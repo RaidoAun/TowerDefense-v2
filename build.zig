096370a2a9a3799412f5ecf2b9b48334f15a14de
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-08-05 20:20:50 +0300

    make check step build tests

--- build.zig
+++ build.zig
@@ -63,20 +63,6 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_check = b.addExecutable(.{
-        .name = "game-check",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe_check.linkLibrary(raylib_artifact);
-    exe_check.root_module.addImport("raylib", raylib);
-    exe_check.root_module.addImport("raygui", raygui);
-
-    const check = b.step("check", "Check if game-check compiles");
-    check.dependOn(&exe_check.step);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -94,4 +80,21 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = "game-check",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_check.linkLibrary(raylib_artifact);
+    exe_check.root_module.addImport("raylib", raylib);
+    exe_check.root_module.addImport("raygui", raygui);
+
+    const check = b.step("check", "Check if game-check compiles");
+    check.dependOn(&exe_check.step);
+    check.dependOn(&exe_unit_tests.step);
+    // if you want to also run the tests then use this one
+    // check.dependOn(&run_exe_unit_tests.step);
 }
